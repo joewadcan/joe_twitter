46f72e20796a9474bec6af2b34e25af413cedd38
Joe Wadcan <joe@wadcan.com>
2012-09-20 17:26:39 -0700

    Modified the full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:sitetitle) {"Joe Twitter"}
   describe "Home page" do
     it "should have the h1 'Joe Twitter'" do
     	visit '/static_pages/home'
@@ -9,9 +10,14 @@ describe "StaticPages" do
 
     it "should have the right title " do
     	visit '/static_pages/home'
-        page.should have_selector("title", :text=> "Joe Twitter | Home")
+        page.should have_selector("title", :text=> "#{sitetitle}")
     end
 
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector("title", :text => "| Home")
+
+    end
 
   end
 
@@ -23,7 +29,7 @@ describe "StaticPages" do
 
     it "should have the right title " do
     	visit '/static_pages/help'
-        page.should have_selector('title', :text=> "Joe Twitter | Help")
+        page.should have_selector('title', :text=> "#{sitetitle} | Help")
     end
   end
 
@@ -35,8 +41,23 @@ describe "StaticPages" do
 
     it "should have the right title " do
     	visit '/static_pages/about'
-        page.should have_selector('title', :text=> "Joe Twitter | About")
+        page.should have_selector('title', :text=> "#{sitetitle} | About")
     end
 
   end
+
+  describe "Contact page" do
+    it "should have the h1 'Contact Me'" do
+    	visit '/static_pages/contact'
+        page.should have_selector('h1', :text=> "Contact Me")
+    end
+
+    it "should have the right title " do
+    	visit '/static_pages/contact'
+        page.should have_selector('title', :text=> "#{sitetitle} | Contact Me")
+    end
+
+  end
+
+
 end
